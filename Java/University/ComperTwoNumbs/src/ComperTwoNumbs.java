0a84792c081d4f3c740777e2ce97efeaf5239fb9
Hitanshu <chaudharyhitanshu@gmail.com>
2020-12-07 22:09:45 +0530

    Add files via upload

---
+++ Java/University/ComperTwoNumbs/src/ComperTwoNumbs.java
@@ -0,0 +1,17 @@
+public class ComperTwoNumbs {
+
+    public static void main(String args[]) {
+
+        //declare two variable
+        int numberOne = 287;
+        int numberTwo = 365;
+
+        if(numberOne > numberTwo) {
+            System.out.println(numberOne+ "is geter then" + numberTwo);
+        } else if (numberOne < numberTwo){
+            System.out.println(numberOne+ "is less than"+ numberTwo);
+        } else {
+            System.out.println(numberOne+ "is equel" + numberTwo);
+        }
+    }
+}
---
+++ Java/University/OddNumbers/src/OddNumbers.java
@@ -0,0 +1,15 @@
+public class OddNumbers {
+    public static void main(String args[]) {
+
+        int upperLimit = 200;
+
+        for (int i = 1; i <= 200; i++) {
+            //System.out.println("Number is " + i);
+
+            if (i % 2 !=0){
+                System.out.println("Number is " + i);
+            }
+        }
+
+    }
+}
